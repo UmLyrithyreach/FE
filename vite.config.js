cd1bbb7c0a32eb69c6490adf4192a88e66471ae1
Kaizen06 <umlyrithyreach@gmail.com>
2025-03-15 20:32:06 +0700

    removing headlessUI

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import headlessUi from '@headlessui/react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    headlessUi(),
   ],
 })
